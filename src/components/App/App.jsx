1240143c84018aed2f33b72d0c6c881403eafc17
808BiTT <bobbitt808@gmail.com>
2023-09-08 22:46:17 -0400

    successfully removed typescript

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import Index from '../Header';
+import ImageGrid from '../ImageGrid';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <>
+            <Index />
+            <ImageGrid />
+        </>
+    );
+};
+
+export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
-import logo from '../assets/logo.svg';
-import '../styles/Header.css';
+import logo from '../../assets/logo.svg';
+
+import './Header.css';
 
 function Header() {
   return (
     <div class="Container">
         <img src={logo} class="Logo" alt="logo" />
-        <h1 class="Title">SolidJS</h1>
+        <h1 class="Title">Bobbitt</h1>
     </div>
   )
 }
